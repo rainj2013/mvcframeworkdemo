3cbff4f3fa06fd2471b5072820cb165a01229bd5
rainj2013 <yangyujian25@gmail.com>
2016-09-18 15:24:59 +0800

    之前怎么提交了个冲突上去

--- src/main/resources/config.js
+++ src/main/resources/config.js
@@ -1,10 +1,6 @@
 {
 path : "/home/rainj2013/桌面/upload",
-<<<<<<< HEAD
 maxFileSize : "1024000",
-=======
-maxFileSize : "1024",
->>>>>>> 18e0838318a743b1e50bdda3057085bce9e3f066
 nameFilter : "^(.+[.])(gif|jpg|png|zip|mp3|txt|mp4|avi|rm|rmvb|rar|bmp|srt|flv|swf|wmv|torrent)$",
 charset : "utf-8"
 }
